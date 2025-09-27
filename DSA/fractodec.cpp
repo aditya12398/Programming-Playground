43e55ff0cb20fd5c859c1e1fff861aab0d647aae
Aditya Prajapati <aditya12398@gmail.com>
2025-09-28 01:17:45 +0530

    Added a timer in the function, got rid of the linear search, used memoisation to reduce compute time

--- DSA/fractodec.cpp
+++ DSA/fractodec.cpp
@@ -1,51 +1,42 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <vector>
+#include <chrono>
 
 class Solution
 {
-private:
-    int find_index(std::vector<int> remainders, int remainder) // Write this function using hash-tables to get a faster result
-    {
-        size_t length;
-        length = remainders.size();
-        int pos = -1;
-        for (int i = 0; i < length; i++)
-        {
-            if (remainder == remainders[i])
-            {
-                pos = i;
-                return pos;
-            }
-        }
-        return pos;
-    }
-
 public:
     std::string fractionToDecimal(int numerator, int denominator)
     {
         std::string decimal, reduced_decimal;
-        int quotient, remainder, rec_start;
+        int quotient, remainder, old_remainder, rec_start = 0;
         std::stringstream ss;
-        std::vector<int> remainders;
+        int *remainders[2];
+        remainders[0] = new int[denominator];
+        remainders[1] = new int[denominator];
         quotient = numerator / denominator;
         remainder = numerator % denominator;
-        remainders.push_back(remainder);
+        remainders[0][remainder] = 1;
+        remainders[1][remainder] = 0;
+        old_remainder = remainder;
         for (int i = 1; i < denominator; i++)
         {
             ss << std::to_string((10 * remainder) / denominator);
-            remainder = 10 * remainders[i - 1] % denominator;
-            rec_start = find_index(remainders, remainder);
-            if (rec_start < 0)
+            remainder = 10 * old_remainder % denominator;
+            
+            if (remainders[0][remainder]!=1)
             {
-                remainders.push_back(remainder);
+                remainders[0][remainder] = 1;
+                remainders[1][remainder] = i;
             }
             else
                 break;
+            
+            old_remainder = remainder;
         }
         // Processing left
         std::string temp = ss.str();
+        rec_start = remainders[1][remainder];
         decimal = std::to_string(quotient) + "." + temp.substr(0, rec_start) + "(" + temp.substr(rec_start) + ")";
         return decimal;
     }
@@ -54,6 +45,7 @@ public:
 int main()
 {
     int n, d;
+    auto start = std::chrono::high_resolution_clock::now();
     std::cout << "Enter Numerator: ";
     std::cin >> n;
     std::cout << "Enter Denominator: ";
@@ -61,5 +53,7 @@ int main()
     Solution s;
     std::string dd;
     dd = s.fractionToDecimal(n, d);
-    std::cout << "Decimal Part: " << dd << "\n";
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+    std::cout << "Decimal Part: " << dd << "\nIn " << duration << " seconds.\n";
 }
